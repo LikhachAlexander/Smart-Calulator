0676bb388bba5269373189e1cd30f0744b57c627
Likhach Alexander <smouzi45@gmail.com>
2020-05-19 23:25:47 +0300

    Simple stuff

--- calculator.py
+++ calculator.py
@@ -1,1 +1,18 @@
 # write your code here
+while True:
+    command = input()
+    if len(command) == 0:
+        continue
+    elif command == '/help':
+        print("The program calculates the sum of numbers")
+        continue
+    elif command == '/exit':
+        print("Bye!")
+        break
+    else:
+        words = command.split()
+        if len(words) > 0:
+            numbers = [int(word) for word in words]
+            print(sum(numbers))
+        else:
+            print(words[0])
