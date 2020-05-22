a0f83949d71f6f75531dc3008879048682808ac6
Likhach Alexander <smouzi45@gmail.com>
2020-05-22 15:05:55 +0300

    Stage 4 linear calculations

--- calculator.py
+++ calculator.py
@@ -1,4 +1,40 @@
-# write your code here
+def calculate(string: str):
+    expression = format_to_expr(string)
+    result = int(expression[0])
+    for i in range(1, len(expression) - 1, 2):
+        sign = expression[i]
+        number = int(expression[i + 1])
+        if sign == '+':
+            result += number
+        if sign == '-':
+            result -= number
+    return result
+
+
+def format_to_expr(string: str):
+    blocks = string.split()
+    expression = []
+    for value in blocks:
+        if '+' in value or '-' in value:
+            expression.append(sign_fix(value))
+        else:
+            expression.append(value)
+    return expression
+
+
+def sign_fix(sign: str) -> str:
+    minus_counter = 0
+    for char in sign:
+        if char == '-':
+            minus_counter += 1
+        elif char != '+':
+            return sign
+    if minus_counter % 2 == 0:
+        return '+'
+    else:
+        return '-'
+
+
 while True:
     command = input()
     if len(command) == 0:
@@ -10,9 +46,4 @@ while True:
         print("Bye!")
         break
     else:
-        words = command.split()
-        if len(words) > 0:
-            numbers = [int(word) for word in words]
-            print(sum(numbers))
-        else:
-            print(words[0])
+        print(calculate(command))
