4bdc49e712d0bcfdbbeb2ca50130c39e1611a377
Likhach Alexander <smouzi45@gmail.com>
2020-06-07 04:03:03 +0300

    Add infix to postfix conversion

--- calculator.py
+++ calculator.py
@@ -1,22 +1,11 @@
-from string import ascii_letters as alphabet
+from collections import deque
 from math import pi, e
 
 
-def sign_fix(sign: str) -> str:
-    minus_counter = 0
-    for char in sign:
-        if char == '-':
-            minus_counter += 1
-        elif char != '+':
-            return sign
-    if minus_counter % 2 == 0:
-        return '+'
-    else:
-        return '-'
-
-
 class Calculator:
-    operands = ['+', '-', '*', '/', '(', ')']
+    operators = ['^', '+', '-', '*', '/']
+    parentheses = ['(', ')']
+    numbers = "0123456789"
 
     def __init__(self):
         # variable storage
@@ -37,7 +26,7 @@ class Calculator:
         result = []
         temp = ""
         for letter in string:
-            if letter not in Calculator.operands:
+            if letter not in Calculator.operators and letter not in Calculator.parentheses:
                 temp += letter
             else:
                 if temp != "":
@@ -51,14 +40,14 @@ class Calculator:
     @staticmethod
     def is_number(string: str) -> bool:
         for letter in string:
-            if letter in alphabet:
+            if letter not in Calculator.numbers + '.':
                 return False
         return True
 
     @staticmethod
     def is_int(string) -> bool:
         for letter in string:
-            if letter in alphabet or letter == '.':
+            if letter not in Calculator.numbers:
                 return False
         return True
 
@@ -71,7 +60,7 @@ class Calculator:
         for i in range(len(array)):
             item = array[i]
             if not function:
-                if item in self.operands:
+                if item in self.operators or item in self.parentheses:
                     result.append(item)
                 elif item in var_names:
                     result.append(self.variables[item])
@@ -108,6 +97,98 @@ class Calculator:
                     function = False
         return result
 
+    @staticmethod
+    def process_signs(array) -> list:
+        result = []
+        minus_counter = 0
+        plus_bool = False
+        for item in array:
+            if item == '+':
+                plus_bool = True
+            elif item == '-':
+                minus_counter += 1
+            else:
+                if plus_bool:
+                    result.append('+')
+                    plus_bool = False
+                if minus_counter > 0:
+                    # append sign
+                    if minus_counter % 2 == 0:
+                        result.append('+')
+                    else:
+                        result.append('-')
+                    minus_counter = 0
+                result.append(item)
+        return result
+
+    @staticmethod
+    def count_parenthesis(array) -> bool:
+        stack = deque()
+        for item in array:
+            if item == '(':
+                stack.append('(')
+            elif item == ')':
+                if len(stack) > 0:
+                    stack.pop()
+                else:
+                    return False
+        if len(stack) > 0:
+            return False
+        else:
+            return True
+
+    @staticmethod
+    def precedence(char: str) -> int:
+        if char == '^':
+            return 3
+        elif char == '*' or char == '/':
+            return 2
+        elif char == '+' or char == '-':
+            return 1
+        else:
+            return 0
+
+    def expression_to_infix(self, string) -> list:
+        expr = Calculator.format_to_infix(string)
+        expr1 = self.fill_values(expr)
+        if expr1 is not None:
+            expr2 = self.process_signs(expr1)
+            return expr2
+        else:
+            return None
+
+    def convert_to_postfix(self, array: list) -> deque:
+        array.append(')')
+        print(array)
+        postfix = deque()
+        stack = deque()
+        stack.append('(')
+        for item in array:
+            if item not in self.operators and item not in self.parentheses:
+                # is operand
+                postfix.append(item)
+            elif item == '(':
+                stack.append(item)
+            elif item == ')':
+                while len(stack) > 0 and stack[-1] != '(':
+                    a = stack.pop()
+                    postfix.append(a)
+                if len(stack) > 0 and stack[-1] != '(':
+                    return None
+                else:
+                    stack.pop()
+
+            # is operator
+            elif item in self.operators:
+                while len(stack) > 0 and Calculator.precedence(item) <= Calculator.precedence(stack[-1]):
+                    postfix.append(stack.pop())
+                stack.append(item)
+        while len(stack) > 0:
+            postfix.append(stack.pop())
+
+        return postfix
+
+
     # calculation functions
     @staticmethod
     def fact(n):
@@ -137,4 +218,4 @@ while True:
         else:
             print("Unknown command")
     else:
-        print(calculator.fill_values(Calculator.format_to_infix(command)))
+        print(calculator.convert_to_postfix( calculator.expression_to_infix(command)))
